8654c384020d3b1bfcd561d657a1f8c9fd02fb92
Dr. Dhaval Joshi <dajoshi@vnsgu.ac.in>
2025-06-14 19:42:37 +0530

    Second

---
+++ MyShopDemo-Microservice/TestProject/Program.cs
@@ -0,0 +1,15 @@
+namespace TestProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
